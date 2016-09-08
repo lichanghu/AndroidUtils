8f50109b4b95950ba7495a5765860e1f3e5c8a0c
master <261204231@qq.com>
2016-09-08 17:25:17 +0800

    增加工具库

--- app/src/main/java/neal/utils/com/androidutils/MainActivity.java
+++ app/src/main/java/neal/utils/com/androidutils/MainActivity.java
@@ -3,11 +3,16 @@ package neal.utils.com.androidutils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import neal.utils.com.library.KLog;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        KLog.d("-------------------------");
     }
 }
---
+++ app/src/main/java/neal/utils/com/androidutils/app/AndroidUtilsApplication.java
@@ -0,0 +1,22 @@
+package neal.utils.com.androidutils.app;
+
+import android.app.Application;
+
+import neal.utils.com.library.KLog;
+
+/**
+ * Created by Neal on 16/9/7.
+ */
+public class AndroidUtilsApplication extends Application {
+
+    public static AndroidUtilsApplication instance;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        //使用AndroidUtils.Klog打印信息 false是关闭信息
+        KLog.init(true);
+
+
+    }
+}
